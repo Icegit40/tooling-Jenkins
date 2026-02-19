8a819d48b48e029d22ba268181cf7c8ba9a549ff
icegit40 <mayeravi2001@gmail.com>
2026-02-19 11:36:33 +0000

    chore: terraform fmt

--- backend.tf
+++ backend.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "tooling/terraform.state"
-    bucket         = "dominion-terraform-backend"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-tooling-state-locking"
+    bucket         = "projectgroup-a-terraform-backend-bucket"
+    region         = "us-east-2"
+    dynamodb_table = "tterraform-state-locking"
   }
 }
